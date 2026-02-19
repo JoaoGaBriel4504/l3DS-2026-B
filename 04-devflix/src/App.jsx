aa32481c25ed496f4932acc7106adb8068858972
JoaoGaBriel4504 <joao.fuzel@aluno.senai.br>
2026-02-19 17:04:54 -0300

    Adiciona funcionalidade de busca de filmes utilizando a API OMDB e implementa o componente MovieCard

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,36 +1,28 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
 import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
-  const [filmes] = useState([
-    {
-      id: 1,
-      titulo: "Série JavaScript",
-      sinopse: "Aprenda JavaScript do zero",
-      genero: "Educação",
-      duracao: 120,
-      imagem: "",
-    },
-    {
-      id: 2,
-      titulo: "React Avançado",
-      sinopse: "Domine React em produção",
-      genero: "Educação",
-      duracao: 180,
-      imagem: "",
-    },
-    {
-      id: 3,
-      titulo: "Web Design Moderno",
-      sinopse: "Crie interfaces incríveis",
-      genero: "Design",
-      duracao: 90,
-      imagem: "",
-    },
-  ]);
+  const[movies, setMovies] = useState([]);
+
+  //Utilazando uma CHAVE de API do arquivo .env
+  const apikey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `http://www.omdbapi.com/?apikey=${apikey}`;
+
+  //Criando a conexão da API e trazendo informações
+const searchMovies = async (title) => {
+  const response = await fetch(`${apiUrl}&s=${title}`);
+  const data = await response.json();
+  
+  //Alimentando a variavel movies
+  setMovies(data.Search);
+};
+
+useEffect(() => {
+  searchMovies("Batman");
+}, []);
 
   return (
     <div>
@@ -46,17 +38,11 @@ const App = () => {
           <img src={lupa} alt="Botão de ação para pesquisa" />
         </div>
 
-        <>
-          {filmes.map((filme, index) => (
-            <div key={index}>
-              <h2>{filme.titulo}</h2>
-              <h3>{filme.duracao}</h3>
-              <h4>{filme.sinopse}</h4>
-              <br />
-              <br />
-            </div>
-          ))}
-        </>
+<div className="container">
+  {movies.map((movie) => (}
+
+</div>
+       
       </div>
       <Rodape link="https://github.com/JoaoGaBriel4504">Joao Gabriel</Rodape>
     </div>
