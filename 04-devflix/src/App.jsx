608ae05771a49a2b4d47fab7714474d3ff325e97
JoaoGaBriel4504 <joao.fuzel@aluno.senai.br>
2026-02-19 15:24:08 -0300

    Adiciona funcionalidade de exibição de filmes na App e implementa estilização para o componente de busca

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,22 +1,63 @@
 import "./App.css";
+import { useState } from "react";
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
 import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
+  const [filmes] = useState([
+    {
+      id: 1,
+      titulo: "Série JavaScript",
+      sinopse: "Aprenda JavaScript do zero",
+      genero: "Educação",
+      duracao: 120,
+      imagem: "",
+    },
+    {
+      id: 2,
+      titulo: "React Avançado",
+      sinopse: "Domine React em produção",
+      genero: "Educação",
+      duracao: 180,
+      imagem: "",
+    },
+    {
+      id: 3,
+      titulo: "Web Design Moderno",
+      sinopse: "Crie interfaces incríveis",
+      genero: "Design",
+      duracao: 90,
+      imagem: "",
+    },
+  ]);
+
   return (
-    <div id="App">
-      <img
-        id="logo"
-        src={logo}
-        alt="Logo da plataforma de streaming DEVFLIX em destaque, com fundo preto e letras vermelhas, otimizado para busca por conteúdo de streaming e entretenimento."
-      />
+    <div>
+      <div id="App">
+        <img
+          id="Logo"
+          src={logo}
+          alt="Logo da plataforma de streaming DEVFLIX em destaque, com fundo preto e letras vermelhas, otimizado para busca por conteúdo de streaming e entretenimento."
+        />
 
-      <div className="search">
-        <input type="text" placeholder="Pesquise por filmes e series..." />
-        <img src={lupa} alt="Botão de ação para pesquisa" />
+        <div className="search">
+          <input type="text" placeholder="Pesquise por filmes e series..." />
+          <img src={lupa} alt="Botão de ação para pesquisa" />
+        </div>
+
+        <>
+          {filmes.map((filme, index) => (
+            <div key={index}>
+              <h2>{filme.titulo}</h2>
+              <h3>{filme.duracao}</h3>
+              <h4>{filme.sinopse}</h4>
+              <br />
+              <br />
+            </div>
+          ))}
+        </>
       </div>
-
       <Rodape link="https://github.com/JoaoGaBriel4504">Joao Gabriel</Rodape>
     </div>
   );
