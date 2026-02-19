529789e35b608bae1c370eb9529f4e368c2d77fd
JoaoGaBriel4504 <joao.fuzel@aluno.senai.br>
2026-02-19 11:19:05 -0300

    Adiciona componente Rodape com estilização e conteúdo, incluindo logo e barra de pesquisa na App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,11 +1,25 @@
-import "./App.css"
+import "./App.css";
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logo da plataforma de streaming DEVFLIX em destaque, com fundo preto e letras vermelhas, otimizado para busca por conteúdo de streaming e entretenimento."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e series..." />
+        <img src={lupa} alt="Botão de ação para pesquisa" />
+      </div>
+
+      <Rodape>Joao Gabriel</Rodape>
+    </div>
+  );
+};
+
+export default App;
